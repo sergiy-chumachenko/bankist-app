de7600592711b52b1da199e6ad79b937d1b0b65b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-06-20 20:34:31 +0300

    create money movements elements

--- script.js
+++ script.js
@@ -56,3 +56,21 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const mv_row = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__date">3 days ago</div>
+        <div class="movements__value">${Math.abs(mov)}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", mv_row);
+  });
+};
+displayMovements(account1.movements);
