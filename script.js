d63027a8a0653a79de20a5c476a69655e6bd52b8
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-07-11 22:11:21 +0300

    'money movements sorting' functionality has been implemented

--- script.js
+++ script.js
@@ -131,9 +131,12 @@ const calcDisplaySummary = function (acc) {
   labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const mv_row = `
       <div class="movements__row">
@@ -217,4 +220,11 @@ btnClose.addEventListener("click", function (e) {
     // Hide UI
     containerApp.style.opacity = 0;
   }
+});
+
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
